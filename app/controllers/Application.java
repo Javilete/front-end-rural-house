2234b6ad8458f168e0fb919f188bba02c74a3895
javier <j.larred@kainos.com>
2014-11-30 23:50:40 +0000

    Create partials, update routes to load them via REST and add html code for main and index for the right loading and inclusion

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,7 +7,28 @@ import views.html.index;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Front end."));
+        return ok(index.render());
     }
 
+/*********************  HOME ******************************/
+    public static Result homeMain() {
+        return ok(views.html.partials.home.main.render());
+    }
+
+    public static Result homeFeatures() {
+        return ok(views.html.partials.home.features.render());
+    }
+
+    public static Result homePictures() {
+        return ok();
+    }
+
+    public static Result homeBooking() {
+        return ok();
+    }
+
+    public static Result homeGetThere() {
+        return ok();
+    }
+/********************* END HOME ******************************/
 }
